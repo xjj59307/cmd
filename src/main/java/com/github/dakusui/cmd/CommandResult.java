a00db35b9b4be542975fd1e67f4dd76515281437
xjj59307 <xjj59307@gmail.com>
2017-04-30 10:32:08 +0900

    Convert spaces to tabs

--- src/main/java/com/github/dakusui/cmd/CommandResult.java
+++ src/main/java/com/github/dakusui/cmd/CommandResult.java
@@ -1,40 +1,38 @@
 package com.github.dakusui.cmd;
 
-
 public class CommandResult {
 
-  private int exitCode;
-  private String commandLine;
-  private String stdout;
-  private String stderr;
-  private String stdouterr;
+	private int exitCode;
+	private String commandLine;
+	private String stdout;
+	private String stderr;
+	private String stdouterr;
 
-  public CommandResult(String commandLine, int exitCode, String stdout, String stderr, String stdouterr) {
-    this.commandLine = commandLine;
-    this.exitCode = exitCode;
-    this.stdout = stdout;
-    this.stderr = stderr;
-    this.stdouterr = stdouterr;
-  }
+	public CommandResult(String commandLine, int exitCode, String stdout, String stderr, String stdouterr) {
+		this.commandLine = commandLine;
+		this.exitCode = exitCode;
+		this.stdout = stdout;
+		this.stderr = stderr;
+		this.stdouterr = stdouterr;
+	}
 
-  public String stdout() {
-    return this.stdout;
-  }
+	public String stdout() {
+		return this.stdout;
+	}
 
-  public String stderr() {
-    return this.stderr;
-  }
+	public String stderr() {
+		return this.stderr;
+	}
 
-  public String stdouterr() {
-    return this.stdouterr;
-  }
+	public String stdouterr() {
+		return this.stdouterr;
+	}
 
-  public int exitCode() {
-    return this.exitCode;
-  }
+	public int exitCode() {
+		return this.exitCode;
+	}
 
-  public String commandLine() {
-    return this.commandLine;
-  }
-
+	public String commandLine() {
+		return this.commandLine;
+	}
 }
