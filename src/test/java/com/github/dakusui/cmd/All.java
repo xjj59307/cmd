f01f8c3752bdf8439e2e0594b78e0110eecefdb1
Hiroshi Ukai <dakusui@users.noreply.github.com>
2017-05-18 08:16:40 +0900

    Revert "Revert "Make it Streamable""

--- src/test/java/com/github/dakusui/cmd/All.java
+++ src/test/java/com/github/dakusui/cmd/All.java
@@ -1,8 +1,9 @@
 package com.github.dakusui.cmd;
 
-import java.net.MalformedURLException;
-import java.util.Set;
-
+import com.github.dakusui.cmd.io.LineReaderTest;
+import com.github.dakusui.cmd.io.RingBufferedLineWriterTest;
+import com.github.dakusui.streamablecmd.ut.CmdTest;
+import com.github.dakusui.streamablecmd.ut.SelectorTest;
 import org.junit.BeforeClass;
 import org.junit.runner.RunWith;
 import org.junit.runners.Suite;
@@ -11,25 +12,29 @@ import org.reflections.scanners.SubTypesScanner;
 import org.reflections.util.ClasspathHelper;
 import org.reflections.util.ConfigurationBuilder;
 
-import com.github.dakusui.cmd.io.LineReaderTest;
-import com.github.dakusui.cmd.io.RingBufferedLineWriterTest;
+import java.net.MalformedURLException;
+import java.util.Set;
 
 @RunWith(Suite.class)
 @Suite.SuiteClasses({
-	LineReaderTest.class,
-	RingBufferedLineWriterTest.class,
-	CommandRunnerTest.class
-})public class All {
-	@BeforeClass public static void loadAllClassesUnderTests() throws MalformedURLException {
-		Reflections reflections = new Reflections(
-				new ConfigurationBuilder()
-				.setUrls(ClasspathHelper.forPackage("com.github.dakusui"))
-				.setScanners(new SubTypesScanner(false))
-		);
-		Set<?> modules = reflections.getSubTypesOf(Object.class);
-		System.out.printf("Loaded %d classes:\n", modules.size());
-		for (Object klass : modules) {
-			System.out.println("    " + klass);
-		}
-	}
+    LineReaderTest.class,
+    RingBufferedLineWriterTest.class,
+    CommandRunnerTest.class,
+    CmdTest.class,
+    SelectorTest.class
+})
+public class All {
+  @BeforeClass
+  public static void loadAllClassesUnderTests() throws MalformedURLException {
+    Reflections reflections = new Reflections(
+        new ConfigurationBuilder()
+            .setUrls(ClasspathHelper.forPackage("com.github.dakusui"))
+            .setScanners(new SubTypesScanner(false))
+    );
+    Set<?> modules = reflections.getSubTypesOf(Object.class);
+    System.out.printf("Loaded %d classes:\n", modules.size());
+    for (Object klass : modules) {
+      System.out.println("    " + klass);
+    }
+  }
 }
